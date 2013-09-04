baa50df7ff133a545c84a676698f865ff14947bf
cotter <cotter.phinney@gmail.com>
2013-09-03 20:34:51 -0400

    init commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,28 @@
+class ItemsController < ApplicationController
+	def index
+		@items = Item.all
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		@item.user = current_user
+		if @item.save
+			flash[:success] = "Your item has been posted!"
+			redirect_to items_path
+		else
+			render 'pages/index'
+		end
+	end
+
+	def item_params
+		params.require(:item).permit(:name, :description, :price)
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trinlist::Application.config.secret_key_base = '15599b3a28b19ae33470ed66c9770b30b2b41e7a675149f56f66bee9d24a6e41dd829ab6291591b11292ea04d76d05ad64a7f157fccf42b4629c1fd288c72278'
