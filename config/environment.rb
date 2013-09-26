51b1b4eb2c9d8089ef6b23c1135f4fb61946dda8
cotter <cotter.phinney@gmail.com>
2013-09-26 13:55:35 -0400

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
-load(app_env_vars) if File.exists?(app_env_vars)
-
 # Initialize the Rails application.
 Trinlist::Application.initialize!
 
