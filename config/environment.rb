741b66d28318bc3edad80c3b230de2c2fd9e459d
cotter <cotter.phinney@gmail.com>
2013-09-26 00:43:05 -0400

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Trinlist::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-   :tls => true,
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => "cotter.phinney",
-   :password => "bigfootgmail"
- }
