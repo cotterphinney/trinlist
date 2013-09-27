6a3bc4eb639c89bd0ac5c1dd3f6f8bbb9ef4b194
cotter <cotter.phinney@gmail.com>
2013-09-27 00:11:33 -0400

    add content type validation to paperclip

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,18 @@
 class Item < ActiveRecord::Base
 	validates_presence_of :name, :description, :location, :price
 	validates_length_of :name, :maximum => 120
-	validates_attachment :image, :size => { :in => 0..3.megabytes }
 	validate :between_one_and_three_categories?
 
 	belongs_to :user
 	acts_as_taggable
 	acts_as_taggable_on :categories
 
-	has_attached_file :image, :styles => { :resized => "250x250>", :large => "600x600>" }, :url => ":s3_domain_url", :path => "/:class/:attachment/:id_partition/:style/:filename"
+	has_attached_file :image, :styles => { :resized => "250x250>", :large => "600x600>" }, 
+		:url => ":s3_domain_url", 
+		:path => "/:class/:attachment/:id_partition/:style/:filename"
+	validates_attachment :image, 
+		:size => { :in => 0..3.megabytes }
+		:content_type => /^image\/(png|gif|jpeg)/
 
 	def between_one_and_three_categories?
 		if ((self.category_list.length == 0) || (self.category_list.length > 3))
