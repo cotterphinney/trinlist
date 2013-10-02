3e92100601901d36437a4919c5412d13d8f8c2bd
cotter <cotter.phinney@gmail.com>
2013-10-01 23:26:06 -0400

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: ENV["COOP_EMAIL"], password: ENV["COOP_PASSWORD"])
